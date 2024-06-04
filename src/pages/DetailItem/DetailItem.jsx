dc667f846a3573bc205b68626f79ea51aee0776e
Lekhacquocbao <quocbao153.iridi77@gmail.com>
2024-06-05 00:40:05 +0700

    39 commit-add best selling detail item

--- src/pages/DetailItem/DetailItem.jsx
+++ src/pages/DetailItem/DetailItem.jsx
@@ -2,8 +2,8 @@ import classNames from 'classnames/bind';
 import axios from 'axios';
 import { Flip, ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { useSpring, animated } from 'react-spring';
-
+import { useSpring } from 'react-spring';
+import { Link } from 'react-router-dom';
 import Button from '~/components/Button';
 import { Icon } from '@iconify/react';
 import { useEffect, useState } from 'react';
@@ -11,81 +11,19 @@ import { useParams } from 'react-router-dom';
 import Rate from '~/components/Rate';
 import GetToken from '~/Token/GetToken';
 import styles from './DetailItem.module.scss';
-import Popup from '~/components/Popup';
-import AutoComplete from '~/components/AutoComplete';
-import InputForm from '~/components/InputForm';
-import { faPhone } from '@fortawesome/free-solid-svg-icons';
+import Slider from 'react-slick';
+
 
 const cx = classNames.bind(styles);
 
 function DetailItem() {
-  const [autocompleteInputValue, setAutocompleteInputValue] = useState('');
-  const [autocompleteInputValue2, setAutocompleteInputValue2] = useState('');
   const { id } = useParams();
   const [count, setCount] = useState(1);
   const [product, setProduct] = useState({});
-  const [shoeSize, setShoeSize] = useState([]);
   const [ratings, setRatings] = useState([]);
   const [idProduct, setIdProduct] = useState();
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [isModalOpen2, setIsModalOpen2] = useState(false);
-
-  const [payload, setPayload] = useState({
-    phoneNumber: '',
-  });
-  const [payload2, setPayload2] = useState({
-    phoneNumber: '',
-  });
-  const modalAnimation = useSpring({
-    opacity: isModalOpen ? 1 : 0,
-  });
-  const modalAnimation2 = useSpring({
-    opacity: isModalOpen2 ? 1 : 0,
-  });
-
-  //Handle Buy with Stripe
-  // const handleBuyStripe = async (address, phoneNumber) => {
-  //   if (!GetToken()) {
-  //     toast.warning('Please login to order !');
-  //   } else {
-  //     try {
-  //       const response = await axios.post(
-  //         'http://localhost:8000/api/payment',
-  //         {
-  //           items: [
-  //             {
-  //               price_data: {
-  //                 currency: 'usd',
-  //                 product_data: {
-  //                   name: shoe.name,
-  //                 },
-  //                 unit_amount: parseInt(((shoe.price / 23000) * 100).toFixed(2)),
-  //               },
-  //               quantity: count,
-  //             },
-  //           ],
-  //           size_items: {
-  //             id_product: idProduct,
-  //             quantity: count,
-  //             price: shoe.price,
-  //           },
-
-  //           address: address,
-  //           phoneNumber: phoneNumber,
-  //         },
-  //         {
-  //           headers: {
-  //             'Content-Type': 'application/json',
-  //             Authorization: `Bearer ${GetToken()}`,
-  //           },
-  //         },
-  //       );
-  //       window.location.replace(response.data.url);
-  //     } catch (err) {
-  //       toast.error('Please try again !');
-  //     }
-  //   }
-  // };
+  const [products, setProducts] = useState([]);
+  const [related, setRelated] = useState([]);
 
   function handleIncrement() {
     setCount(count + 1);
@@ -122,73 +60,46 @@ function DetailItem() {
     }
   };
 
-  // const handleCreateOneOrder = async (item, address, phoneNumber) => {
-  //   if (!GetToken()) {
-  //     toast.warning('Please login to order!');
-  //   } else {
-  //     try {
-  //       const response = await axios.post(
-  //         'http://localhost:8000/api/order/createOneItem',
-  //         {
-  //           Item: {
-  //             id_product: idProduct,
-  //             quantity: count,
-  //             price: item.price,
-  //           },
-  //           address: address,
-  //           phoneNumber: phoneNumber,
-  //         },
-  //         {
-  //           headers: {
-  //             'Content-Type': 'application/json',
-  //             Authorization: `Bearer ${GetToken()}`,
-  //           },
-  //         },
-  //       );
-
-  //       if (response.data.success) {
-  //         toast.success(response.data.message);
-  //         setTimeout(() => {
-  //           window.location.reload();
-  //         }, 2000);
-  //       } else {
-  //         toast.error(response.data.message);
-  //       }
-  //     } catch (error) {
-  //       toast.error('Quantity error !');
-  //     }
-  //   }
-  // };
+  const settingSlider = {
+    // dots: true,
+    infinite: true,
+    speed: 400,
+    slidesToShow: 6,
+    slidesToScroll: 2,
+  };  
 
   useEffect(() => {
     const getAPIDetailItem = async () => {
       const response = await axios.get(`http://localhost:8000/api/product/${id}`);
       setProduct(response.data.result);
-      // setShoeSize(response.data.result.Size_items);
       setRatings(response.data.result.rating);
-      console.log("respone detail item ne hehe", response)
-      if (response.data.result.Size_items && response.data.result.Size_items.length > 0) {
-        setIdProduct(response.data.result.Size_items[0].id);
+      // if (response.data.result.Size_items && response.data.result.Size_items.length > 0) {
+      //   setIdProduct(response.data.result.Size_items[0].id);
+      // }
+    };
+
+    const fetchAPIProducts = async () => {
+      try {
+        const response = await axios.get('http://localhost:8000/api/revenue/product');
+        // console.log('besst selling', response);
+        setProducts(response.data.result.products);
+      } catch (error) {        console.log(error);
       }
     };
 
+    const fetchAPIRelaredProducts = async () => {
+      try {
+        const response = await axios.get('http://localhost:8000/api/product');
+        console.log('related', response);
+        setRelated(response.data.result);
+      } catch (error) {
+        console.log(error);
+      }
+    };
     getAPIDetailItem();
+    fetchAPIProducts();
+    fetchAPIRelaredProducts();
   }, [id]);
-
-  // const openModal = () => {
-  //   setIsModalOpen(true);
-  // };
-
-  // const closeModal = () => {
-  //   setIsModalOpen(false);
-  // };
-  // const openModal2 = () => {
-  //   setIsModalOpen2(true);
-  // };
-
-  // const closeModal2 = () => {
-  //   setIsModalOpen2(false);
-  // };
 
   function formatCurrency(number) {
     const formatter = new Intl.NumberFormat('vi-VN', {
@@ -219,36 +130,11 @@ function DetailItem() {
         </div>
         <div className={cx('prouduct-info')}>
           <h1 className={cx('product-name')}>{product.name}</h1>
+          <p className={cx('product-des')}>
+            {product.Breed ? product.Breed.name : 'Breed information is not available'}
+          </p>
           <p className={cx('product-des')}>{product.description}</p>
           <span className={cx('prouduct-price')}>{formatCurrency(product.price)}</span>
-          {/* <div className={cx('product-cs')}>
-            <div className={cx('product-color')}>
-              <div className={cx('title')}>COLOR</div>
-              <div className={cx('color')} style={{ backgroundColor: `${shoe.color}` }}></div>
-            </div>
-            <div className={cx('product-size')}>
-              <div className={cx('title')}>SIZE</div>
-              <select
-                className={cx('size')}
-                onChange={(e) => {
-                  const selectedSizeId = e.target.value;
-                  setIdProduct(selectedSizeId);
-                }}
-                value={idProduct}
-              >
-                {shoeSize &&
-                  shoeSize.map((size) => {
-                    return (
-                      <>
-                        <option key={size.id} className={cx('option')} value={size.id}>
-                          {size.size} - {size.amount} Product
-                        </option>
-                      </>
-                    );
-                  })}
-              </select>
-            </div>
-          </div> */}
           <div className={cx('product-cart')}>
             <div className={cx('product-quantity')}>
               <Icon className={cx('minus')} icon="typcn:minus" onClick={handleDecrement} />
@@ -266,13 +152,6 @@ function DetailItem() {
               <Icon className={cx('plus')} icon="typcn:plus" onClick={handleIncrement} />
             </div>
 
-            {/* <button className={cx('custom-btn', 'btn-5')} onClick={() => openModal()}>
-              <span>BUY NOW</span>
-            </button>
-            <button className={cx('custom-btn', 'btn-5')} onClick={() => openModal2()}>
-              <span>STRIPE</span>
-            </button> */}
-
             <button
               className={cx('custom-btn', 'btn-5')}
               onClick={() => {
@@ -284,58 +163,62 @@ function DetailItem() {
           </div>
         </div>
       </div>
-      {/* <Popup isOpen={isModalOpen} onRequestClose={() => closeModal()} width={String('500px')} height={'350px'}>
-        <animated.div style={modalAnimation}>
-          <h2>Payment confirmation</h2>
-          <div className={cx('input-field')}>
-            <div className={cx('header')}>Enter address</div>
-            <AutoComplete setParentInputValue={setAutocompleteInputValue} />
-          </div>
-          <div className={cx('input-field')}>
-            <div className={cx('header')}>Enter phone number</div>
-            <InputForm
-              placeholder={'Enter phone number'}
-              type="text"
-              value={payload.phoneNumber}
-              setValue={setPayload}
-              name={'phoneNumber'}
-              className={cx('input')}
-              leftIcon={faPhone}
-            />
-          </div>
-          <div className={cx('options')}>
-            <Button onClick={() => handleCreateOneOrder(shoe, autocompleteInputValue, payload.phoneNumber)} outline>
-              Confirm
-            </Button>
-          </div>
-        </animated.div>
-      </Popup> */}
-      {/* <Popup isOpen={isModalOpen2} onRequestClose={() => closeModal2()} width={String('500px')} height={'350px'}>
-        <animated.div style={modalAnimation2}>
-          <h2>Payment confirmation</h2>
-          <div className={cx('input-field')}>
-            <div className={cx('header')}>Enter address</div>
-            <AutoComplete setParentInputValue={setAutocompleteInputValue2} />
-          </div>
-          <div className={cx('input-field')}>
-            <div className={cx('header')}>Enter phone number</div>
-            <InputForm
-              placeholder={'Enter phone number'}
-              type="text"
-              value={payload2.phoneNumber}
-              setValue={setPayload2}
-              name={'phoneNumber'}
-              className={cx('input')}
-              leftIcon={faPhone}
-            />
-          </div>
-          <div className={cx('options')}>
-            <Button onClick={() => handleBuyStripe(autocompleteInputValue2, payload2.phoneNumber)} outline>
-              Confirm
-            </Button>
-          </div>
-        </animated.div>
-      </Popup> */}
+
+      <div className={cx('categories')}>
+        <h2 className={cx('header')}>BEST SELLING PRODUCTS</h2>
+        <Slider {...settingSlider}>
+        {products &&
+            products.map((products) => {
+              return (
+                <div className={cx('category')}>
+                  <Link >
+                    <img className={cx('category-image')} src={products.image} alt="products"></img>
+                    <div className={cx('category-container')}>
+                      <div className={cx('category-title')}>{products.name}</div>
+                      <Button
+                        key={products.id}
+                        animation
+                        className={cx('category-btn')}
+                        to={`/detailItem/${products.id}`}
+                      >
+                        SHOP NOW
+                      </Button>
+                    </div>
+                  </Link>
+                </div>
+              );
+            })}
+        </Slider>
+      </div>
+      
+
+      <div className={cx('categories')}>
+        <h2 className={cx('header')}>RELATED PRODUCTS</h2>
+        <Slider {...settingSlider}>
+        {related &&
+            related.map((products) => {
+              return (
+                <div className={cx('category')}>
+                  <Link>
+                    <img className={cx('category-image')} src={products.image} alt="products"></img>
+                    <div className={cx('category-container')}>
+                      <div className={cx('category-title')}>{products.name}</div>
+                      <Button
+                        key={products.id}
+                        animation
+                        className={cx('category-btn')}
+                        to={`/detailItem/${products.id}`}
+                      >
+                        SHOP NOW
+                      </Button>
+                    </div>
+                  </Link>
+                </div>
+              );
+            })}
+        </Slider>
+      </div>  
+
       <div className={cx('extra-detail')}>
         <div className={cx('header-field')}>
           <span className={cx('header-rate')}>Rating</span>
