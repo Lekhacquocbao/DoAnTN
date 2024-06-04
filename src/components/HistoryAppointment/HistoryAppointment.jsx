425505662d6827418c3070060c8781cd5efe694b
Lekhacquocbao <quocbao153.iridi77@gmail.com>
2024-06-04 14:48:11 +0700

    35 commit-done appointment history

--- src/components/HistoryAppointment/HistoryAppointment.jsx
+++ src/components/HistoryAppointment/HistoryAppointment.jsx
@@ -1,12 +1,11 @@
 import classNames from 'classnames/bind';
 import axios from 'axios';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useSpring, animated } from 'react-spring';
 import moment from 'moment';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Flip, ToastContainer, toast } from 'react-toastify';
 import { faCancel } from '@fortawesome/free-solid-svg-icons';
-
 import GetToken from '~/Token/GetToken';
 import Image from '~/components/Image';
 import Button from '~/components/Button';
@@ -18,13 +17,40 @@ const cx = classNames.bind(styles);
 function HistoryAppointment({ data, icon }) {
   const [appointmentDetail, setAppointmentDetail] = useState({});
   const [isModalOpenDetail, setIsModalOpenDetail] = useState(false);
+
+  const [formData, setFormData] = useState({
+    note: data.note,
+    appointment_time: data.appointment_time, 
+    end_time: data.end_time,
+  });
   const modalAnimationDetail = useSpring({
     opacity: isModalOpenDetail ? 1 : 0,
   });
 
+  useEffect(() => {
+    setFormData({
+      note: data.note,
+      appointment_time: data.appointment_time,
+      end_time: data.end_time,
+    });
+  }, [data]); 
+
   if (!data) {
     return null;
   }
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value
+    }));
+  };
+
+  const orderStartTime = data.appointment_time;
+  const orderEndTime = data.end_time;
+  const formattedStartTime = moment(orderStartTime).format('YYYY-MM-DD HH:mm:ss');
+  const formattedEndTime = moment(orderEndTime).format('YYYY-MM-DD HH:mm:ss');
 
   const openModalDetail = async (id) => {
     setIsModalOpenDetail(true);
@@ -37,32 +63,8 @@ function HistoryAppointment({ data, icon }) {
     setAppointmentDetail(response.data.detailAppointment);
   };
 
-
   const closeModalDetail = () => {
     setIsModalOpenDetail(false);
-  };
-
-  const handleChangeAppointmentAccept = async (id) => {
-    await axios
-      .put(
-        `http://localhost:8000/api/appointment/accept/${id}`,
-        {},
-        {
-          headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${GetToken()}`,
-          },
-        },
-      )
-      .then((res) => {
-        toast.success(res.data.message);
-        setTimeout(() => {
-          window.location.reload();
-        }, 2000);
-      })
-      .catch((e) => {
-        toast.success(e);
-      });
   };
 
   const handleChangeAppointmentCancel = async (id) => {
@@ -88,10 +90,28 @@ function HistoryAppointment({ data, icon }) {
       });
   };
 
-  const orderDate = data.appointment_time;
-  const orderEndTime = data.end_time;
-  const formattedDate = moment(orderDate).format('YYYY-MM-DD HH:mm:ss');
-  const formattedEndTime = moment(orderEndTime).format('YYYY-MM-DD HH:mm:ss');
+  const handleUpdate = async (id) => {
+    await axios
+      .put(
+        `http://localhost:8000/api/appointment/update/${id}`,
+        formData,
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${GetToken()}`, // Điều chỉnh nếu cần
+          },
+        },
+      )
+      .then((res) => {
+        toast.success(res.data.message);
+        setTimeout(() => {
+          window.location.reload();
+        }, 2000);
+      })
+      .catch((e) => {
+        toast.error(e.response ? e.response.data.message : 'An error occurred');
+      });
+  };
 
   let iconComponent;
   let buttonComponent;
@@ -107,10 +127,6 @@ function HistoryAppointment({ data, icon }) {
           outline
         >
           Get Detail
-        </Button>
-
-        <Button onClick={() => handleChangeAppointmentAccept(data.id)} className={cx('btn')} blue>
-          Accept
         </Button>
 
         <Button onClick={() => handleChangeAppointmentCancel(data.id)} className={cx('btn')} blue>
@@ -131,10 +147,6 @@ function HistoryAppointment({ data, icon }) {
         >
           Get Detail
         </Button>
-
-        {/* <Button onClick={() => handleChangeAppointment(data.id)} className={cx('btn')} blue>
-          Confirm
-        </Button> */}
       </div>
     );
   } else if (data.id_status === 5) {
@@ -150,14 +162,10 @@ function HistoryAppointment({ data, icon }) {
         >
           Get Detail
         </Button>
-
-        {/* <Button onClick={() => handleChangeAppointment(data.id)} className={cx('btn')} blue>
-          Confirm
-        </Button> */}
       </div>
     );
   }
-
+  
   function formatCurrency(number) {
     const formatter = new Intl.NumberFormat('vi-VN', {
       style: 'currency',
@@ -180,14 +188,14 @@ function HistoryAppointment({ data, icon }) {
         pauseOnHover
         theme="light"
       />
-      <Image className={cx('order-image')} src={data.Account.inforUser.avatar} alt="avatar"></Image>
+      {/* <Image className={cx('order-image')} src={data.Account.inforUser.avatar} alt="avatar"></Image> */}
       {iconComponent}
-      <div className={cx('name-order')}>{data.Account.inforUser.firstname + ' ' + data.Account.inforUser.lastname}</div>
+      {/* <div className={cx('name-order')}>{data.Account.inforUser.firstname + ' ' + data.Account.inforUser.lastname}</div> */}
       <div className={cx('name-order')}>{data.note}</div>
+      <div className={cx('day-order')}>{formattedStartTime}</div>
       <div className={cx('day-order')}>{formattedEndTime}</div>
-      <div className={cx('day-order')}>{formattedDate}</div>
       <div className={cx('price-order')}>{data.totalPrice && formatCurrency(data.totalPrice)}</div>
-     
+
       {buttonComponent}
       <Popup
         className={cx('modal-container')}
@@ -203,7 +211,11 @@ function HistoryAppointment({ data, icon }) {
           {/* return ( */}
           <div className={cx('detail')}>
             {/* <Image className={cx('detail-image')} src={data.Account.inforUser.avatar} alt="avatar"></Image> */}
-            <Image className={cx('detail-image')} src={appointmentDetail.Service ? appointmentDetail.Service.image : ''} alt="avatar"></Image>
+            <Image
+              className={cx('detail-image')}
+              src={appointmentDetail.Service ? appointmentDetail.Service.image : ''}
+              alt="avatar"
+            ></Image>
             <div className={cx('detail-item')}>
               <label className={cx('detail-label')}>Name:</label>
               <div className={cx('detail-value')}>
@@ -214,39 +226,68 @@ function HistoryAppointment({ data, icon }) {
             <div className={cx('detail-item')}>
               <label className={cx('detail-label')}>Service:</label>
               <div className={cx('detail-value')}>
-              {appointmentDetail.Service ? appointmentDetail.Service.name : ''}
+                {appointmentDetail.Service ? appointmentDetail.Service.name : ''}
               </div>
             </div>
 
             <div className={cx('detail-item')}>
               <label className={cx('detail-label')}>Description:</label>
-              <div className={cx('detail-value')}>{appointmentDetail.Service ? appointmentDetail.Service.description : ''}</div>
+              <div className={cx('detail-value')}>
+                {appointmentDetail.Service ? appointmentDetail.Service.description : ''}
+              </div>
             </div>
 
             <div className={cx('detail-item')}>
               <label className={cx('detail-label')}>Note:</label>
-              <div className={cx('detail-value')}>{data.note}</div>
+              <input
+                className={cx('detail-input')}
+                name="note"
+                value={formData.note}
+                onChange={handleInputChange}
+              />
+            </div>
+
+
+            <div className={cx('detail-item')}>
+              <label className={cx('detail-label')}>Start Time:</label>
+              <div className={cx('detail-value')}>{formattedStartTime}</div>
             </div>
 
             <div className={cx('detail-item')}>
-              <label className={cx('detail-label')}>Start Date:</label>
-              <div className={cx('detail-value')}>{formattedDate}</div>
+              <label className={cx('detail-label')}>Update Start Time:</label>
+              <input
+                className={cx('detail-value-input')}
+                name="appointmentTime"
+                type="datetime-local"
+                // value={formData.appointmentTime}
+                onChange={handleInputChange}
+              />
             </div>
 
             <div className={cx('detail-item')}>
-              <label className={cx('detail-label')}>End Date:</label>
+              <label className={cx('detail-label')}>End Time:</label>
               <div className={cx('detail-value')}>{formattedEndTime}</div>
             </div>
 
-            {/* <div className={cx('detail-item')}>
-              <label className={cx('detail-label')}>Price service:</label>
-              <div className={cx('detail-value')}>{appointmentDetail.Service ? appointmentDetail.Service.price : ''}</div>
-            </div> */}
+            <div className={cx('detail-item')}>
+              <label className={cx('detail-label')}>Update End Time:</label>
+              <input
+                className={cx('detail-value')}
+                name="endTime"
+                type="datetime-local"
+                // value={formData.endTime}
+                onChange={handleInputChange}
+              />
+            </div>
 
             <div className={cx('detail-item')}>
               <label className={cx('detail-label')}>Price:</label>
               <div className={cx('detail-value')}>{data.totalPrice && formatCurrency(data.totalPrice)}</div>
             </div>
+
+            <Button onClick={() => handleUpdate(data.id)} className={cx('btn')} blue>
+              Update
+            </Button>
           </div>
           {/* ); */}
           {/* })} */}
